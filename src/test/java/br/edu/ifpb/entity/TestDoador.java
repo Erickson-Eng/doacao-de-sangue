f96ee9651f69cd9016e8e9817798cd81789781b8
Erickson Azevedo <erickson.tulio96@gmail.com>
2021-09-14 23:18:01 -0300

    Readme

--- src/test/java/br/edu/ifpb/entity/TestDoador.java
+++ src/test/java/br/edu/ifpb/entity/TestDoador.java
@@ -8,6 +8,7 @@ public class TestDoador {
     @Test
     public void criaDoador(){
         Doador doador = new Doador();
+        Doador falsoDoador = new Doador();
         doador.setName("Erickson");
         doador.setPeso(85.4);
         doador.setDataNascimento("19/01/1996");
@@ -15,5 +16,11 @@ public class TestDoador {
         doador.setEstaDoente(false);
         doador.setNovoDoador(false);
 
+        doador.setName("ABC123");
+        doador.setPeso(1.1);
+        doador.setDataNascimento("19/01/1800");
+        doador.setGenero(GeneroEnum.FEMININO);
+        doador.setEstaDoente(true);
+        doador.setNovoDoador(true);
     }
 }
